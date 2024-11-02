95e3677fd0b4eda576c7718764bae865c1e17ab2
y-ookuma <ysk.bigbear@gmail.com>
2024-11-02 18:57:04 +0900

    Create py_sample.sh

---
+++ opt/uecs2influxdbV2/py_sample.sh
@@ -0,0 +1,2 @@
+#/bin/bash -c 'source /home/pi/myenv/bin/activate && exec python /opt/uecs2influxdbV2/uecs2influxdb.py'
+#/bin/bash -c 'source /home/pi/myenv/bin/activate && exec python /opt/uecs2influxdbV2/abc_aggregate.py'
