87b9581c79ee41e8dfe704de6d93a0978768221a
y-ookuma <ysk.bigbear@gmail.com>
2024-11-03 19:58:33 +0900

    Delete opt/uecs2influxdbV2/py_sample.sh

--- opt/uecs2influxdbV2/py_sample.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash -c 'source /home/pi/myenv/bin/activate && exec python /opt/uecs2influxdbV2/uecs2influxdb.py'
-#/bin/bash -c 'source /home/pi/myenv/bin/activate && exec python /opt/uecs2influxdbV2/abc_aggregate.py'
